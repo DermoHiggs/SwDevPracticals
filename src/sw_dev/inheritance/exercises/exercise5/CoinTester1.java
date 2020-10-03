2e82e1d036cc16ef8da2bf0cebe757d517f8993b
Dermot Hegarty LYIT <dermot.hegarty@lyit.ie>
2020-10-03 17:12:14 +0100

    Solutions to inheritance exercises

---
+++ src/sw_dev/inheritance/exercises/exercise5/CoinTester1.java
@@ -0,0 +1,28 @@
+package sw_dev.inheritance.exercises.exercise5;
+
+public class CoinTester1 {
+    public static void main(String[] args) {
+        Coin c1 = new Coin(10, "Ten Cent");
+        Coin c2 = new Coin(10, "Ten Cent");
+        Coin c3 = new Coin(50, "Fifty Cent");
+        //c2=c1;
+
+        if(c1.equals(c2))
+        {
+            System.out.println("c1 and c2 are equal");
+        }
+        else
+        {
+            System.out.println("c1 and c2 are not equal");
+        }
+
+        if(c3.equals(c2))
+        {
+            System.out.println("c3 and c2 are equal");
+        }
+        else
+        {
+            System.out.println("c3 and c2 are not equal");
+        }
+    }
+}
